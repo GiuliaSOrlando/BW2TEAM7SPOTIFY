74aedb6f860a719fb34f72b94f13346562af25f0
GiuliaSOrlando <134381769+GiuliaSOrlando@users.noreply.github.com>
2023-07-11 07:48:40 +0200

    amend5

--- player/player.js
+++ player/player.js
@@ -7,3 +7,23 @@ const repeatBtn = document.getElementById("repeat-button")
 repeatBtn.addEventListener("click", function () {
   repeatBtn.classList.toggle("active-btn")
 })
+
+const aboutBtn = document.getElementById("about-button")
+aboutBtn.addEventListener("click", function () {
+  aboutBtn.classList.toggle("active-btn")
+})
+
+const queueBtn = document.getElementById("queue-button")
+queueBtn.addEventListener("click", function () {
+  queueBtn.classList.toggle("active-btn")
+})
+
+const connectBtn = document.getElementById("connect-button")
+connectBtn.addEventListener("click", function () {
+  connectBtn.classList.toggle("active-btn")
+})
+
+const volumeBtn = document.getElementById("volume-button")
+volumeBtn.addEventListener("click", function () {
+  volumeBtn.classList.toggle("active-btn")
+})

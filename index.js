a2747075b78421aed5d356c61caa8a7ed6e00e22
GiuliaSOrlando <134381769+GiuliaSOrlando@users.noreply.github.com>
2023-07-11 16:47:14 +0200

    Index first draft

--- index.js
+++ index.js
@@ -1,10 +1,131 @@
+// CODICE NON LEGATO ALL'API
+
 document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach((tooltip) => {
   new bootstrap.Tooltip(tooltip)
 })
 
-const albumUrl =
+let greetingText = document.getElementById("greeting-message")
+const timeNow = new Date().getHours()
+let greeting =
+  timeNow >= 5 && timeNow < 12
+    ? "Buongiorno"
+    : timeNow >= 12 && timeNow < 18
+    ? "Buon pomeriggio"
+    : "Buonasera"
+greetingText.innerText = `${greeting}`
+
+//FETCH
+
+const QueenUrl =
   "https://striveschool-api.herokuapp.com/api/deezer/search?q=queen"
 
+const rockUrl =
+  "https://striveschool-api.herokuapp.com/api/deezer/search?q=rock"
+
+// Creo le tessere sotto il saluti
+const populateGreetings = function (elements) {
+  for (let i = 0; i < 6; i++) {
+    const greetingsRow = document.getElementById("greetings-row")
+    const newCol = document.createElement("div")
+    newCol.classList.add("col", "mb-2")
+    newCol.innerHTML = `
+      <div class="card p-0 border-0">
+                    <div
+                      class="row row-cols-2 row-cols-md-3 h-25 align-items-center colo p-0 m-0"
+                    >
+                      <div class="col p-0">
+                        <img
+                          src="${elements.data[i].album.cover_medium}"
+                          class="img-fluid rounded-start"
+                          alt="..."
+                        />
+                      </div>
+                      <div class="col-md-8 d-flex">
+                        <div class="card-body">
+                          <h5 class="card-title text-white">${elements.data[i].album.title}</h5>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+      `
+
+    greetingsRow.appendChild(newCol)
+  }
+}
+
+// Creo la sezione con quattro album
+const populateAlbums = function (elements) {
+  for (let i = 6; i < 10; i++) {
+    const albumRow = document.getElementById("album-row")
+    const newCol = document.createElement("div")
+    newCol.classList.add("col-md-3")
+    newCol.innerHTML = `
+                            <div class="card text-white">
+                        <div id="c-img">
+                          <img
+                            src="${elements.data[i].album.cover_medium}"
+                            class="card-img-top img-album"
+                            alt="Immagine 1"
+                          />
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="48"
+                            height="48"
+                            viewBox="0 0 24 24"
+                            fill="#1db954"
+                            stroke-width="1"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            class="feather feather-play-circle play-card"
+                          >
+                            <circle cx="12" cy="12" r="10"></circle>
+                            <polygon
+                              points="10 8 16 12 10 16 10 8"
+                              fill="black"
+                            ></polygon>
+                          </svg>
+                        </div>
+                        <div class="card-body text-white">
+                          <h5 class="card-title">${elements.data[i].album.title}</h5>
+                          <h6 class="card-subtitle mb-2">${elements.data[i].artist}</h6>
+                        </div>
+                      </div>
+      `
+
+    albumRow.appendChild(newCol)
+  }
+}
+
+// Creo la lista nella libreria
+const populateLibrary = function (elements) {
+  for (let i = 0; i < 15; i++) {
+    const libraryDeck = document.getElementById("library-deck")
+    const newCol = document.createElement("div")
+    newCol.classList.add("card", "card-libreria")
+    newCol.innerHTML = `
+                                                           <div class="row no-gutters">
+                        <div class="col-3">
+                          <img
+                            src="${elements.data[i].album.cover_small}"
+                            class="card-img-top img-libreria flex-column"
+                            alt="Album 1"
+                          />
+                        </div>
+                        <div class="col-9 p-0">
+                          <div class="card-body p-0 flex-row">
+                            <p class="card-title text-white">${elements.data[i].album.title}</p>
+                            <p class="card-text text-white">${elements.data[i].artist.name}</p>
+                          </div>
+                        </div>
+                      </div>
+      `
+
+    libraryDeck.appendChild(newCol)
+  }
+}
+
+// Funzoine generica per la fetch
 const getData = function (url, foo) {
   fetch(url)
     .then((res) => {
@@ -14,12 +135,30 @@ const getData = function (url, foo) {
         throw new Error("Errore nella chiamata")
       }
     })
-    .then((data) => {
-      foo(data)
+    .then((elements) => {
+      console.log(elements)
+      foo(elements)
     })
     .catch((err) => {
       console.log(err)
     })
 }
 
-getData(albumUrl, console.log)
+getData(QueenUrl, populateGreetings)
+getData(QueenUrl, populateAlbums)
+getData(rockUrl, populateLibrary)
+
+const albumBtn = document.getElementById("album-btn")
+const artistBtn = document.getElementById("artist-btn")
+
+albumBtn.addEventListener("click", () => {
+  const libraryDeck = document.getElementById("library-deck")
+  libraryDeck.innerHTML = ""
+  getData(rockUrl, populateLibrary)
+})
+
+artistBtn.addEventListener("click", () => {
+  const libraryDeck = document.getElementById("library-deck")
+  libraryDeck.innerHTML = ""
+  getData(QueenUrl, populateLibrary)
+})

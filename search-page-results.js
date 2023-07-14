8d14d8fb6fcd738e873cba2e3da0f736137d4a5c
Mirko Parente <133377396+mirkoparente@users.noreply.github.com>
2023-07-14 09:56:15 +0200

    search-page

--- search-page-results.js
+++ search-page-results.js
@@ -1,5 +1,12 @@
 // CODICE NON LEGATO ALL'API
-
+for (let e of document.querySelectorAll(
+  'input[type="range"].slider-progress'
+)) {
+  e.style.setProperty("--value", e.value);
+  e.style.setProperty("--min", e.min == "" ? "0" : e.min);
+  e.style.setProperty("--max", e.max == "" ? "100" : e.max);
+  e.addEventListener("input", () => e.style.setProperty("--value", e.value));
+}
 document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach((tooltip) => {
   new bootstrap.Tooltip(tooltip)
 })

dae4e092c2a6bba9b85da5ae7ac6fb6c0f58c57c
Mirko Parente <133377396+mirkoparente@users.noreply.github.com>
2023-07-14 10:31:16 +0200

    player

--- album-page.js
+++ album-page.js
@@ -1,3 +1,21 @@
+//codice non legato API
+for (let e of document.querySelectorAll(
+  'input[type="range"].slider-progress'
+)) {
+  e.style.setProperty("--value", e.value);
+  e.style.setProperty("--min", e.min == "" ? "0" : e.min);
+  e.style.setProperty("--max", e.max == "" ? "100" : e.max);
+  e.addEventListener("input", () => e.style.setProperty("--value", e.value));
+}
+
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach((tooltip) => {
+  new bootstrap.Tooltip(tooltip);
+});
+
+
+
+
+
 let playing = false
 
 const QueenUrl =
